23b8ce00dd22bfeb650e4edbd6e2c4daed0726a8
kllkrk <142680519+kllkrk@users.noreply.github.com>
2023-08-21 19:21:27 +0300

    Add files via upload

---
+++ responsiivinen_menu.js
@@ -0,0 +1,8 @@
+function ylaMenu() {
+    var x = document.getElementById("ylanavi");
+    if (x.className === "ylanavi") {
+      x.className += " responsiivinen";
+    } else {
+      x.className = "ylanavi";
+    }
+  }
